97d71615016d9affa220bae6a369c1547d16774b
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-12-05 19:28:59 -0300

    First Version

---
+++ javascript/common functions.js
@@ -0,0 +1,59 @@
+function createTitle(highlight, extraClasses) {
+    var className = "mb-3"
+    if (!!extraClasses) {
+        className += " " + extraClasses
+    }
+    const text = createCustomElement("h2", className);
+    text.innerHTML = highlight.name;
+    return text
+}
+
+function createDescription(highlight) {
+    const text = createElement("p");
+    text.innerHTML = highlight.description;
+    return text
+}
+
+function createTextGroup(title, body) {
+    const group = createCustomElement("div", "text-group mb-2");
+
+    const titleElement = createCustomElement("span", "font-weight-bold");
+    titleElement.innerHTML = title;
+
+    const bodyElement = createElement("span");
+    bodyElement.innerHTML = body;
+
+    group.appendChild(titleElement);
+    group.appendChild(bodyElement);
+    return group;
+}
+
+function createImageLink(path, url, label, spacing = false) {
+    var className = "";
+    if (spacing) {
+        className += " ml-2";
+    }
+
+    const link = createCustomElement("a", className)
+    link.href = url;
+    link.target = "_blank";
+
+    const img = createCustomElement("img", "availableOn");
+    img.src = path;
+    img.alt = label;
+
+    link.appendChild(img);
+
+    return link;
+}
+
+function createElement(elementName) {
+    const element = document.createElement(elementName);
+    return element;
+}
+
+function createCustomElement(elementName, className) {
+    const element = document.createElement(elementName);
+    element.className = className;
+    return element;
+}
---
+++ javascript/highlights.js
@@ -0,0 +1,49 @@
+function displayHighlight() {
+    const section = document.getElementById("highlights");
+
+    for (var i = 0; i < highlights.length; i++) {
+        const row = createCustomElement("div", "row mb-5");
+        createHighlightCard(highlights[i], row);
+        section.appendChild(row);
+    }
+}
+
+function createHighlightCard(highlight, row) {
+    const imageBox = createCustomElement("div", "col-xs-12 col-lg-6")
+    const image = createHighlightImage(highlight);
+    imageBox.appendChild(image);
+
+    const textContainer = createCustomElement("div", "col-xs-12 col-lg-6 d-flex flex-column justify-content-between");
+    const textBox = createElement("div");
+
+    const title = createTitle(highlight);
+    textBox.appendChild(title);
+
+    const desc = createDescription(highlight);
+    textBox.appendChild(desc);
+
+    const linkBox = createElement("div");
+    const technologies = createTextGroup("Technologies: ", highlight.technologies);
+    linkBox.appendChild(technologies);
+
+    if (!!highlight.appStoreLink) {
+        const appStore = createImageLink("assets/Available on the App Store.png", highlight.appStoreLink, "App Store");
+        linkBox.appendChild(appStore);
+    }
+    if (!!highlight.gitHubLink) {
+        const github = createImageLink("assets/Available on GitHub.png", highlight.gitHubLink, "GitHub", !!highlight.appStoreLink);
+        linkBox.appendChild(github);
+    }
+    textContainer.appendChild(textBox);
+    textContainer.appendChild(linkBox);
+
+    row.appendChild(imageBox);
+    row.appendChild(textContainer);
+}
+
+function createHighlightImage(highlight) {
+    const img = createCustomElement("img", "width100");
+    img.src = highlight.imagePath;
+    img.alt = highlight.name + " Icon";
+    return img
+}
